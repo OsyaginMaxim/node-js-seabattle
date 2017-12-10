dd44d98bf2af1285cc98c18e34cc6545c027b33e
Maxim Osyagin <i.am.dream5896@gmail.com>
2017-12-10 13:41:12 +0300

    create hit test

--- public/js/my-scripts.js
+++ public/js/my-scripts.js
@@ -1,24 +1,7 @@
 var socket = io();
-var isnotkillship = function(point){
-    var p = JSON.parse(point, function (key, value) {
-        if(key === 'pound'){
-            //вытаскиваем значение value в массив, вытаскиваем оттуда значения i,j;
-            //сравниваем значения в p1map[i][j] с 's', если совпало, то
-            //засчитываем попадание и возвращаем false
-            //иначе возвращаем true
-        }
 
-    });
-    concole.log(p);
-};
 
 (function(w,h) {
-
-
-
-    /*let p1map = ['~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~'];
-    let p2map = ['~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~~~','~~~~~~~~s~'];
-*/
 
     let p1map = ["~", "~", "s", "~", "~", "~", "~", "~", "~", "~",
         "~", "~", "~", "~", "~", "~", "~", "~", "~", "~",
@@ -42,34 +25,56 @@ var isnotkillship = function(point){
         "~", "~", "~", "~", "~", "~", "~", "~", "~", "~",
         "~", "~", "~", "~", "~", "~", "~", "~", "~", "~"];
 
+    var isnotkillship = function(point){
+        var p = JSON.parse(point);
+        //console.log(p);
+        var str = p.pound;
+        //console.log(str);
+        var arr = str.split('');
+        var i1 = arr.slice(0,1);
+        var j1 = arr.slice(2,3);
+        if(p1map[i1][j1] === 's'){
+            //console.log(p1map[i1][j1]);
+            return false;
+        }else{
+            //console.log(p1map[i1][j1]);
+            return true;
+        }
+
+    };
+
     let p1 = document.querySelector("#battlefield1");//.player1 > #battlefield1');
     let p2 = document.querySelector("#battlefield2");//.player2 > #battlefield2');
-    console.log(p2map);
+    //console.log(p2map);
     for (i = 0;i < w;i++)
         for (j = 0;j < h;j++){
             div1 = document.createElement("div");
             div1.id = i + '-' +j + '-' + '1';
             div1.className = (p1map[i][j] === 's') ? 's': 'w'; //p1map[i*w+j]
+            //на div1 нужно поставить обработчик onclick,
+            // который будет устанавливать значения палуб т.е. p1map[i][j] = 's';
             p1.appendChild(div1);
             div2 = document.createElement("div");
-            console.log(p2map[1]);
-                   // p2map.splice(1,1,'~'); - удаление эл-та на месте i=0,j=1 и замена его на другое значение
-
+            // p2map.splice(1,1,'~'); - удаление эл-та на месте i=0,j=1 и замена его на другое значение
             div2.className = (p2map[i][j] === 's') ? 's': 'w'; //p2map[i*w+j]
             div2.id = i + '_' +j + '-' + '2';
             p2.appendChild(div2);
             div2.onclick = function () {
                 let ship = {};
                 ship ['pound'] = this.id;
-                console.log(ship);
-                socket.emit('fire', ship);
-
-
+                //console.log(ship);
+                let stringJSON = JSON.stringify(ship);
+                //console.log(stringJSON);
+                socket.emit('fire', stringJSON);
                 socket.on('isKill', function (data) {
+                    console.log(isnotkillship(data));
                     if(isnotkillship(data)){
                         socket.emit('notkill', data);
+                        //console.log(data);
                     }
+                    //console.log(data);
                     socket.emit('kill', data);
+
                 });
 
 
